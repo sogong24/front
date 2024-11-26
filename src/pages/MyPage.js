b8372fcb2d8c9cf1925f4df9c89b9607a93ea48a
juhyun <wngus5861@naver.com>
2024-11-26 12:40:22 +0900

    FEAT: mypage.css 추가

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,3 +1,9 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./Mypage.css";
+
+
+
 function Mypage() {
     return (
         <div className="p-4">
