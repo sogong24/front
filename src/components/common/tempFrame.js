399100a8a9c7e9d86a5a7945d98d1f220e94f464
yccs-26 <yuchan001016@gmail.com>
2024-11-28 01:58:26 +0900

    search 파일 구조 고려

--- src/components/common/tempFrame.js
+++ src/components/common/tempFrame.js
@@ -5,7 +5,6 @@ function TempFrame() {
         <div className='fixed top-0 left-0 w-full h-full bg-gray-100 bg-poacity-75 flex justify-center items-center'>
             <div className='bg-white p-4 rounded shadow-lg'>
                 <h2 className='text-lg font-bold mb-4'>임시 프레임</h2>
-
             </div>
         </div>
     );
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,7 +1,18 @@
+import Searchbar from '../components/search/SearchBar';
+import SearchResults from '../components/search/SearchResults.js';
+import SearchFilters from '../components/search/SearchFilters.js';
+
 function SearchPage() {
     return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold">검색</h1>
+        <div className='p-4'>
+            <h1 className='text-2xl font-bold'>검색</h1>
+            <div className='space-y-4'>
+                {/* 검색 체크박스 - 학년&학기, 강의명, 교수명 */}
+                {/* 입력 가능 + 체크박스 가능 */}
+                <SearchBar />
+                <SearchFilters />
+                <SearchResults />
+            </div>
         </div>
     );
 }
