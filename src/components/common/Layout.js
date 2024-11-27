5e3e460c03981bbc554e9a1441234b02323eb059
yccs-26 <yuchan001016@gmail.com>
2024-11-28 01:39:11 +0900

    temp_frame

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,9 +1,10 @@
 import Menubar from './Menubar';
-
+import TempFrame from './tempFrame';
 function Layout({ children }) {
     return (
         <div className='w-[700px] min-h-screen mx-auto relative pb-16 bg-[#f5f5f5]'>
             {children}
+            <TempFrame />
             <Menubar />
         </div>
     );
--- src/components/common/Menubar.js
+++ src/components/common/Menubar.js
@@ -10,6 +10,18 @@ function Menubar() {
     return (
         <div className='fixed bottom-0 w-[700px] h-16 bg-white border-t border-gray-200'>
             <div className='flex justify-around items-center'>
+
+                {/* 게시글 작성 메뉴 */}
+                <Link to="/post/create" className={`flex flex-col items-center ${isActive('/post/create')}`}>
+                    <div className="w-6 h-6 mb-1">
+                        <div className="w-full h-full flex items-center justify-center">
+                            <div className="w-4 h-0.5 bg-current absolute" />
+                            <div className="w-0.5 h-4 bg-current absolute" />
+                        </div>
+                    </div>
+                    <span className="text-xs">게시글 작성</span>
+                </Link>
+
                 {/* 홈 메뉴 */}
                 <Link to="/" className={`flex flex-col items-center ${isActive('/')}`}>
                     <div className='w-6 h-6 mb-1'>
@@ -26,17 +38,7 @@ function Menubar() {
                     <span className="text-xs">검색</span>
                 </Link>
 
-                {/* 게시글 작성 메뉴 */}
-                <Link to="/post/create" className={`flex flex-col items-center ${isActive('/post/create')}`}>
-                    <div className="w-6 h-6 mb-1">
-                        <div className="w-full h-full flex items-center justify-center">
-                            <div className="w-4 h-0.5 bg-current absolute" />
-                            <div className="w-0.5 h-4 bg-current absolute" />
-                        </div>
-                    </div>
-                    <span className="text-xs">게시글 작성</span>
-                </Link>
-
+                {/* 마이페이지 */}
                 <Link to="/mypage" className={`flex flex-col items-center ${isActive('/mypage')}`}>
                     <div className="w-6 h-6 mb-1">
                         <div className="w-full h-full rounded-full border-2 border-current "/>
