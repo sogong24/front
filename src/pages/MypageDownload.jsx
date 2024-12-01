ed2713d9d0db7018fb78a3ad85fac86df090a60b
juhyun <wngus5861@naver.com>
2024-12-01 15:22:45 +0900

    FEAT: 마이페이지-다운받은목록 및 작성한 목록 뼈대 구현

--- src/pages/MypageDownload.jsx
+++ src/pages/MypageDownload.jsx
@@ -6,10 +6,10 @@ import { GoDownload } from "react-icons/go";
 export default function MypageDownload() {
     return (
         <div className="mypageDownload">
-            <div className="mypageDownload-container">1
-                <div className="mypageDownload-container-inf">소프트웨어 공학</div><GoDownload /></div>
-            <div className="mypageDownload-container">2<GoDownload /></div>
-            <div className="mypageDownload-container">3<GoDownload /></div>
+            <div className="mypageDownload-container">1  
+                <div className="mypageDownload-container-inf">소프트웨어 공학 정지은 pf 2024학년도 2학기</div><GoDownload className="download-icons"/></div>
+            <div className="mypageDownload-container">2<GoDownload className="download-icons"/></div>
+            <div className="mypageDownload-container">3<GoDownload className="download-icons"/></div>
         </div>
     );
 }
--- src/pages/MypageWrite.jsx
+++ src/pages/MypageWrite.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./MypageWrite.css";
+import { TfiWrite } from "react-icons/tfi";
+
+
 
 export default function MypageWrite() {
     return (
-        <div className="write">
-            <h1 className="text-2xl font-bold">작성한 목록</h1>
+        <div className="mypagewrite">
+            <div className="mypagewrite-container">
+            <div className="mypagewrite-container-inf">1 소프트웨어 공학 정지은 pf<TfiWrite />
+            </div>
+            </div>
         </div>
     );
 }
