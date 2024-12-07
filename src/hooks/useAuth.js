e5e33572843e070ada0f242d98b36e65aa9e016e
Kim chanwoo <cw020218@naver.com>
2024-12-07 21:27:35 +0900

    feat: addlogin and signup features

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,51 +1,66 @@
-import { useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
 
 function useAuth() {
-    // const [isLoggedIn, setIsLoggedIn] = useState(false);
-    // DB 연동 전 로그인 체크---------------------------------
-    const [isLoggedIn, setIsLoggedIn] = useState(() => {
-        return localStorage.getItem('isLoggedIn') === 'true';
-    });
-    //--------------------------------------------------------
-    const navigate = useNavigate();
-    const location = useLocation();
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // DB 연동 전 로그인 체크---------------------------------
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    return localStorage.getItem("token") === "true";
+  });
+  //--------------------------------------------------------
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const noBarPaths = ['/login', '/signup'];
+  const noBarPaths = ["/login", "/signup"];
 
-    const login = () => {
-        console.log('로그인 함수 실행');
-        setIsLoggedIn(true);
-        // DB 연동 전 로그인 체크---------------------------------
-        localStorage.setItem('isLoggedIn', 'true');
-        navigate('/home');
-    };
-
-    const logout = () => {
-        setIsLoggedIn(false);
-        // localStorage - DB 연동 전 삭제 ; 임시 로직
-        localStorage.removeItem('isLoggedIn');
-        navigate('/login');
-    };
-
-    // DB 연동 전 로그인 체크
-    const checkAuth = () => {
-        const isAuthenticated = localStorage.getItem('isLoggedIn') === 'true';
-        if (!isAuthenticated && location.pathname !== '/login') {
-            return false;
+  const login = async (email, password) => {
+    console.log("로그인 함수 실행");
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/users/login",
+        {
+          email,
+          password,
         }
-        return true;
-    };
+      );
+      const { token } = response.data;
+      setIsLoggedIn(true);
+      console.log("토큰 : ", token);
+      localStorage.setItem("token", token);
+      // DB 연동 전 로그인 체크---------------------------------
+      localStorage.setItem("isLoggedIn", "true");
+      setIsLoggedIn(true);
+      console.log("로그인 성공");
+      alert("로그인에 성공했습니다.");
+      navigate("/home");
+    } catch (error) {
+      console.error(
+        "로그인 실패:",
+        error.response?.data?.error || error.message
+      );
+      alert("로그인에 실패했습니다. 이메일과 비밀번호를 확인하세요.");
+    }
+  };
 
-    // DB 연동 후 사용할 로직
-    // const checkAuth = () => {
-    //     if(!isLoggedIn && !noBarPaths.includes(location.pathname)) {
-    //         return false;
-    //     }
-    //     return true;
-    // };
+  const logout = () => {
+    setIsLoggedIn(false);
+    // localStorage - DB 연동 전 삭제 ; 임시 로직
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    navigate("/login");
+  };
 
-    return { isLoggedIn, login, logout, checkAuth, noBarPaths };
+  // DB 연동 전 로그인 체크
+  const checkAuth = () => {
+    const isAuthenticated = localStorage.getItem("isLoggedIn") === "true";
+    if (!isAuthenticated && location.pathname !== "/login") {
+      return false;
+    }
+    return true;
+  };
+
+  return { isLoggedIn, login, logout, checkAuth, noBarPaths };
 }
 
 export default useAuth;
